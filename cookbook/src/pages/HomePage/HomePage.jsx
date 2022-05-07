4ac523bab188f21e3ca6893e85906494c2c3eae8
NensyN <nenanikolic07@gmail.com>
2022-05-07 16:48:01 +0200

    skill part

--- cookbook/src/pages/HomePage/HomePage.jsx
+++ cookbook/src/pages/HomePage/HomePage.jsx
@@ -7,6 +7,7 @@ import fork from '../../images/fork.png'
 import pies from '../../images/pies.PNG'
 import soup from '../../images/soup.jpg'
 import oatmeal from '../../images/oatmeal.jpg'
+import skills from '../../images/skills.jpg'
 
 
 const HomePage = () => {
@@ -28,26 +29,35 @@ const HomePage = () => {
                 <div className='secmain2'>
                     <div className='sm21'>
                         <img src={pies} alt="burger" className='p1' />
-                        <p> <span>food</span> <br />
+                        <p className='littletitle'>FOOD</p>
+                        <p>  
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sunt voluptas eum iure quas odio ipsum distinctio cumque ut modi. Sequi, ipsa repellendus quae tempore debitis ratione molestias ut dicta, aliquid iure earum placeat optio tenetur provident voluptatibus reprehenderit itaque.</p>
                     </div>
 
                     <div className='sm21'>
                         <img src={soup} alt="salad" className='p1' />
-                        <p> <span>food</span> <br />
+                        <p className='littletitle'>FOOD</p>
+                        <p>  
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sunt voluptas eum iure quas odio ipsum distinctio cumque ut modi. Sequi, ipsa repellendus quae tempore debitis ratione molestias ut dicta, aliquid iure earum placeat optio tenetur provident voluptatibus reprehenderit itaque.</p>
                     </div>
 
                     <div className='sm21'>
                         <img src={oatmeal} alt="pancakes" className='p1' />
-                        <p> <span>food</span> <br />
+                        <p className='littletitle'>FOOD</p>
+                        <p>  
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sunt voluptas eum iure quas odio ipsum distinctio cumque ut modi. Sequi, ipsa repellendus quae tempore debitis ratione molestias ut dicta, aliquid iure earum placeat optio tenetur provident voluptatibus reprehenderit itaque.</p>
                     </div>
 
                 </div>
             </div>
 
-
+            <div className='skilldiv'>
+                <div className='pictureskill'></div>
+              <div className='skillnote'>
+                 <p className='title'>SKILLS</p>
+                 <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo animi repellendus provident perspiciatis magnam eos obcaecati sunt fugit, unde veritatis error numquam et, nisi aspernatur aut. Consectetur deserunt exercitationem suscipit.</p>
+              </div>
+            </div>
 
 
 
